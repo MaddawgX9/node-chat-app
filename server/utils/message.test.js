6f6c1889f70fa9d42a0557158b0ad803d565f3a3
Dolan Gish <maddawgx9@comcast.net>
2018-08-20 18:35:36 -0700

    Add css for chat page

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage',() => {
     it('should generate the correct message object',() => {
@@ -9,6 +9,20 @@ describe('generateMessage',() => {
         var msg = generateMessage(from,text);
 
         expect(msg.createdAt).toBeA('number');
-        expect(msg).toInclude({from,text});        
+        expect(msg).toInclude({from,text});
     });
-})
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from="Dolan";
+        var lng=1;
+        var lat=2;
+        var url='https://www.google.com/maps?q=2,1';
+
+        var msg = generateLocationMessage(from, lat, lng);
+
+        expect(msg.createdAt).toBeA('number');
+        expect(msg).toInclude({from,url});
+    });
+});
